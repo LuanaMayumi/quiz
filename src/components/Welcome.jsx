6286b5387541af77114277cbbe320de8e7f4cdfe
LuanaMayumi <luh.mayumi@gmail.com>
2023-01-20 12:23:28 -0300

    adicionou funcao dispatch no component Welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -8,14 +8,14 @@ import '../assets/styles/Welcome.css';
 import Quiz from '../assets/img/quiz.svg';
 
 const Welcome = () => {
-  const quizState = useContext(QuizContext);
+  const [quizState, dispatch] = useContext(QuizContext); // quizState pega os valores, dispatch altera o state
   console.log(quizState);
 
   return (
     <div id='welcome'>
       <h2> Seja bem-vindo </h2>
       <p>Clique no botão abaixo para começar:</p>
-      <button>Iniciar</button>
+      <button onClick={() => dispatch({ type: 'CHANGE_STATE'})}>Iniciar</button>
       <img src={Quiz} alt='Inicio do Quiz'></img>
     </div>
   )
